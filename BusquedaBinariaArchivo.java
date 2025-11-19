4ee6748a2b93cfc191bc6f3e59d973d7ff8075a4
RalphHellisng62 <arenasrafael889@gmail.com>
2025-11-19 17:15:49 -0600

    Cronstrucción del main y un array

--- BusquedaBinariaArchivo.java
+++ BusquedaBinariaArchivo.java
@@ -1,3 +1,22 @@
+import java.util.Arrays;
+
 public class BusquedaBinariaArchivo {
-    
+    // Iniciamos con la construcción de un main
+
+    public static void main(String[] args) {
+        String archivoEntrada = "numeros.txt";
+        String archivoSalida = "numeros_ordenados.txt";
+
+        int[] numeros = leerArchivo(archivoEntrada);
+
+        if (numeros != null) {
+            System.out.println("numeros originales:");
+            imprimirArray(numeros);
+
+            // aqui se ordenaran los datos
+            Arrays.sort(numeros);
+
+            System.out.println("\n numeros ordenados:");
+            imprimirArray(numeros);
+    }
 }
